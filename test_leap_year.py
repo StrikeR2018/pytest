188d0f9a097441462b5d037859626f4a54066aed
Mingxuan Li <52895636+StrikeR2018@users.noreply.github.com>
2021-05-28 04:58:49 -0700

    Update test_leap_year.py

--- test_leap_year.py
+++ test_leap_year.py
@@ -4,13 +4,13 @@ import leap_year
 
 class TestCase(unittest.TestCase):
     def test_input_1(self):
-        self.assertEqual(leap_year.check_user_input(2020), 2020)
+        self.assertEqual(leap_year.check_user_input(2000), 2000)
     
     def test_check_1(self):
-        self.assertEqual(leap_year.check_leap_year(2020), 0)
+        self.assertEqual(leap_year.check_leap_year(2000), 2)
     
     def test_check_2(self):
-        self.assertEqual(leap_year.check_leap_year(2021), -1)
+        self.assertEqual(leap_year.check_leap_year(2000), 1)
 
 if __name__ == "__main__":
-    unittest.main(verbosity=2)
+    unittest.main()
